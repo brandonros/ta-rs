647bf8327f73cfd919f57d2c12e827af0e4486f8
Brandon Ros <brandonros1@gmail.com>
2023-02-12 14:54:45 -0500

    missing crosses

--- src/indicators/trend_magic.rs
+++ src/indicators/trend_magic.rs
@@ -6,7 +6,10 @@ use super::{TrueRange, SimpleMovingAverage, CommodityChannelIndex};
 
 pub struct TrendMagic {
   atr_multiplier: f64,
-  previous: f64,
+  previous_trend_magic: f64,
+  previous_low: f64,
+  previous_high: f64,
+  previous_close: f64,
   tr: TrueRange,
   tr_sma: SimpleMovingAverage,
   cci: CommodityChannelIndex
@@ -16,7 +19,10 @@ impl TrendMagic {
   pub fn new(atr_periods: usize, atr_multiplier: f64, cci_periods: usize) -> Self {
       Self {
         atr_multiplier,
-        previous: 0.0,
+        previous_trend_magic: 0.0,
+        previous_close: 0.0,
+        previous_high: 0.0,
+        previous_low: 0.0,
         tr: TrueRange::new(),
         tr_sma: SimpleMovingAverage::new(atr_periods).unwrap(),
         cci: CommodityChannelIndex::new(cci_periods).unwrap()
@@ -24,8 +30,26 @@ impl TrendMagic {
   }
 }
 
+fn crossover(previous_x: f64, previous_y: f64, current_x: f64, current_y: f64) -> bool {
+  let condition1 = previous_x <= previous_y;
+  let condition2 = current_x >= current_y;
+  return condition1 && condition2;
+}
+
+fn crossunder(previous_x: f64, previous_y: f64, current_x: f64, current_y: f64) -> bool {
+  let condition1 = previous_x >= previous_y;
+  let condition2 = current_x <= current_y;
+  return condition1 && condition2;
+}
+
+fn cross(previous_x: f64, previous_y: f64, current_x: f64, current_y: f64) -> bool {
+  let crossover_result = crossover(previous_x, previous_y, current_x, current_y);
+  let crossunder_result = crossover(previous_x, previous_y, current_x, current_y);
+  return crossover_result || crossunder_result;
+}
+
 impl<T: High + Low + Close> Next<&T> for TrendMagic {
-  type Output = (f64, f64, f64);
+  type Output = (bool, bool, bool);
 
   fn next(&mut self, input: &T) -> Self::Output {
     // tr and then sma the tr to act like an alternative to atr
@@ -37,20 +61,33 @@ impl<T: High + Low + Close> Next<&T> for TrendMagic {
     // up or down
     let up = input.low() - atr * self.atr_multiplier;
     let down = input.high() + atr * self.atr_multiplier;
-    if cci >= 0.0 {
-      if up < self.previous {
-        
+    let current_trend_magic = if cci >= 0.0 {
+      if up < self.previous_trend_magic {
+        self.previous_trend_magic
       } else {
-        self.previous = up;
+        up
       }
     } else {
-      if down > self.previous {
-        
+      if down > self.previous_trend_magic {
+        self.previous_trend_magic
       } else {
-        self.previous = down;
+        down
       }
-    }
-    return (up, down, self.previous);
+    };
+    // crosses
+    let cross1 = cross(input.close(), current_trend_magic, self.previous_close, self.previous_trend_magic);
+    let cross2 = crossover(input.low(), current_trend_magic, self.previous_low, self.previous_trend_magic);
+    let cross3 = crossunder(input.high(), current_trend_magic, self.previous_high, self.previous_trend_magic);
+    // update state
+    self.previous_trend_magic = current_trend_magic;
+    self.previous_close = input.close();
+    self.previous_high = input.high();
+    self.previous_low = input.low();
+    return (
+      cross1,
+      cross2,
+      cross3
+    )
   }
 }
 
@@ -68,7 +105,10 @@ impl fmt::Display for TrendMagic {
 
 impl Reset for TrendMagic {
   fn reset(&mut self) {
-    self.previous = 0.0;
+    self.previous_close = 0.0;
+    self.previous_high = 0.0;
+    self.previous_low = 0.0;
+    self.previous_trend_magic = 0.0;
     self.tr.reset();
     self.tr_sma.reset();
     self.cci.reset();
